bd8087d241928e47f761d7384f22e578a0ec9099
Sobham Sandilya <sandilyasobham@gmail.com>
2026-03-30 00:47:20 +0530

    UI bugs fixed, UI formatted for mobile usage, favorites not resetting after logout fixed, search page debugged

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -74,8 +74,7 @@ sm:group-hover:translate-y-0
  <button
   onClick={() => onPlayTrailer(movie.id)}
   className="
-    absolute inset-0
-    flex items-center justify-center
+    absolute bottom-20 right-12
     opacity-100 sm:opacity-0 sm:group-hover:opacity-100
     transition duration-300
   "
